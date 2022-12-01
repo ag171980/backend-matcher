87e6b918fc2eac6280274d9afa9a8d14a749c5f1
ag171980 <ag171980@gmail.com>
2022-12-01 10:04:15 -0300

    a'

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import db from "./database/db.js"
 import { PORT } from "./config.js"
 const app = express()
 
-// app.use("/", userRoutes)
+app.use("/", userRoutes)
 app.use("/", testRoutes)
 
 /* app.use("/registrarme",blogRegister) */
