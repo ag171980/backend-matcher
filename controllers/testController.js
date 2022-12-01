9169677d02d0339c9244193450887cd63e114585
ag171980 <ag171980@gmail.com>
2022-12-01 01:40:19 -0300

    a

--- controllers/testController.js
+++ controllers/testController.js
@@ -5,6 +5,7 @@ import TestModel from "../models/TestModel.js";
 export const getTest = async (req, res) => {
     try {
         const test = await TestModel.findAll()
+        console.log(test)
         res.json(test)
     } catch (error) {
         res.json({ message: error.message })
