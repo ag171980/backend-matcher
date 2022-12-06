ad903fd385ca545cb4255e89b86540cd672ed5f8
ag171980 <ag171980@gmail.com>
2022-12-06 02:40:22 -0300

    a " "a"

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -31,7 +31,7 @@ export const getMatchById = async (req, res) => {
 export const verifyMatchesUserById = async (req, res)=>{
     let idUserEntry = 1;
     try{
-        const usersLikedsByEntry = await UserModel.findAll({where:{id: idUserEntry}});
+        const usersLikedsByEntry = await UserModel.findAll();
         const allMatches = await MatchModel.findAll();
         let filterMatchesById = allMatches.filter((match)=>match.id_user_matchA === idUserEntry || match.id_user_matchB === idUserEntry)
         let usersMatches = []
