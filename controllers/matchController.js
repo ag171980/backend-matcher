a82e3186aa1e2cd2fae9456a03b0132f37027354
ag171980 <ag171980@gmail.com>
2022-12-06 04:48:48 -0300

    a

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -42,41 +42,34 @@ export const verifyMatchesUserById = async (req, res)=>{
         const allMatches = await MatchModel.findAll();
         const userActual = await UserModel.findAll({where:{id: idUserEntry}})
         const filterMatchesById = await MatchModel.findAll({where: 
-            {
-                [Op.or]: [
+            {[Op.or]: [
                     {
-                        id_user_matchA: 
-                        {
-                            [Op.eq]: Number(idUserEntry)
-                        }
+                        id_user_matchA: {[Op.eq]: Number(idUserEntry)}
                     }, 
                     {
-                        id_user_matchB: 
-                        {
-                            [Op.eq]: Number(idUserEntry)
-                        }
+                        id_user_matchB: {[Op.eq]: Number(idUserEntry)}
                     }
                 ]
             }
             })
         // let filterMatchesById = allMatches.filter((match)=>match.id_user_matchA === idUserEntry || match.id_user_matchB === idUserEntry)
-        // let usersMatches = []
+        let usersMatches = []
         
-        // filterMatchesById.map((match, index)=>{
-        //     let idActualA = match.id_user_matchA;
-        //     let idActualB = match.id_user_matchB;
-        //     let userByIdB = users.filter((us)=>us.id === idActualB)[0].name
-        //     filterMatchesById.map((matchJ, jndex)=>{
-        //         if(matchJ.id_user_matchB === idActualA && matchJ.id_user_matchA === idActualB){
-        //             if(userByIdB !== userActual){
-        //                 let dataUserToMatch = users.filter((user)=>user.name === userByIdB)
-        //                 usersMatches.push(dataUserToMatch)
-        //             }
-        //             // eliminarItemEspecifico(userActual, usersMatches)
-        //         }
-        //     })
+        filterMatchesById.map((match, index)=>{
+            let idActualA = match.id_user_matchA;
+            let idActualB = match.id_user_matchB;
+            let userByIdB = users.filter((us)=>us.id === idActualB)[0].name
+            filterMatchesById.map((matchJ, jndex)=>{
+                if(matchJ.id_user_matchB === idActualA && matchJ.id_user_matchA === idActualB){
+                    if(userByIdB !== userActual[0].name){
+                        let dataUserToMatch = users.filter((user)=>user.name === userByIdB)
+                        usersMatches.push(dataUserToMatch)
+                    }
+                    // eliminarItemEspecifico(userActual, usersMatches)
+                }
+            })
 
-        // })
+        })
         res.json(filterMatchesById)
     }catch(error){
         res.json({message: error.message })
